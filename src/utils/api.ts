992fe98570689c6acb988caae04ca34f2e6cd507
m-hodges <michaelcshodges@gmail.com>
2021-09-07 00:14:16 +1200

    fix domains/get csrf token setting

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "http://localhost:8000",
   withCredentials: true,
 });
 
@@ -10,8 +10,7 @@ export async function authenticateCSRF() {
 }
 
 export async function asyncSignin(email: string, password: string) {
-  const res = await authenticateCSRF();
-  console.log(res.headers["XSRF-TOKEN"]);
+  await authenticateCSRF();
   return await api.post("/login", {
     email,
     password,
